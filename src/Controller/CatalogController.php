26ae3e41d3443b4d23d7158984ae2ec125052ee5
Rodolphe <dutchr@live.fr>
2022-04-27 11:11:34 +0200

    WIP connexion to access catalog

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Type;
+use App\Repository\TypeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CatalogController extends AbstractController
 {
     #[Route('/catalog', name: 'catalog')]
     public function index(TypeRepository $repository , EntityManagerInterface $entityManager): Response
     {
+      $types = $entityManager->getRepository(Type::class)->findAll();
         return $this->render('catalog/index.html.twig', [
-            'controller_name' => 'CatalogController',
+            'types' => $types,
         ]);
     }
 }
+ 
